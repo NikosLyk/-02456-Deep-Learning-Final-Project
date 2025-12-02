99724edfaab67e65eda56d3ea02ee8996be5d17a
Nadia Leal <89272655+nadialeal@users.noreply.github.com>
2025-12-02 11:10:04 +0100

    Add files via upload
    unet arch tests

---
+++ unet_full.py
@@ -0,0 +1,77 @@
+import torch
+from torch import nn
+
+class DoubleConv(nn.Module):
+    """(conv => ReLU) * 2"""
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class UNetLike(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+
+        # Encoder
+        self.enc1 = DoubleConv(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+        self.enc2 = DoubleConv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+        self.enc3 = DoubleConv(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+        self.enc4 = DoubleConv(256, 512)
+        self.pool4 = nn.MaxPool2d(2)
+
+        # Bottleneck
+        self.bottleneck = DoubleConv(512, 1024)
+
+        # Decoder
+        self.up4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.dec4 = DoubleConv(1024, 512)
+
+        self.up3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec3 = DoubleConv(512, 256)
+
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = DoubleConv(256, 128)
+
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = DoubleConv(128, 64)
+
+        # Final 1x1
+        self.out_conv = nn.Conv2d(64, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.enc1(x)
+        x2 = self.enc2(self.pool1(x1))
+        x3 = self.enc3(self.pool2(x2))
+        x4 = self.enc4(self.pool3(x3))
+
+        x5 = self.bottleneck(self.pool4(x4))
+
+        x = self.up4(x5)
+        x = torch.cat([x4, x], dim=1)
+        x = self.dec4(x)
+
+        x = self.up3(x)
+        x = torch.cat([x3, x], dim=1)
+        x = self.dec3(x)
+
+        x = self.up2(x)
+        x = torch.cat([x2, x], dim=1)
+        x = self.dec2(x)
+
+        x = self.up1(x)
+        x = torch.cat([x1, x], dim=1)
+        x = self.dec1(x)
+
+        logits = self.out_conv(x)
+        return logits
